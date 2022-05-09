aa0030a188eb8ec9566442dd55d865c058b1c7d2
taka0713 <tyunisu2018@gmail.com>
2022-05-09 23:15:12 +0900

    20220509

--- problem4.js
+++ problem4.js
@@ -3,7 +3,7 @@ let numbers = (sample_input.trim()).split("\n");
 let n = numbers.length;
 
 let a,b,c,d,e,f = 0;
-for(let z = 0; z < n; z += 1){
+for(let z = 0; z < n; z ++){
      [a,b,c,d,e,f] = numbers[z].split(' ');
      y=(a*f-d*c)/(e*a-d*b);
      x=(c-b*y)/a;
