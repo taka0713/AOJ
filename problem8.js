410571073d5e6c6ed01a8b73a2064ab959ac81eb
taka0713 <tyunisu2018@gmail.com>
2022-05-18 00:10:08 +0900

    20220518

--- problem8.js
+++ problem8.js
@@ -1,12 +1,13 @@
 let n = require('fs').readFileSync('/dev/stdin', 'utf8');
 let numbers = (n.trim()).split("\n").map(Number);
-let c = 0;
+
 for(n = 0 ; n < numbers.length; n++){
+    let c = 0;
         for(i = 0 ; i <= 9; i++){
             for(j = 0 ; j <= 9; j++){
                 for(k = 0 ; k <= 9; k++){
                     for(l = 0 ; l <= 9; l++){
-                        if(i + j + k + l === n){
+                        if(i + j + k + l === numbers[n]){
                             c++
                         }
                     }
