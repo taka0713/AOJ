d8145d24a8903e008748248ea3339e8085802243
taka0713 <tyunisu2018@gmail.com>
2022-05-20 01:15:51 +0900

    20220520

---
+++ problem48.js
@@ -0,0 +1,29 @@
+let n = require('fs').readFileSync('/dev/stdin', 'utf8');
+let numbers = (n.trim()).split("\n");
+
+for (let i = 0; i < numbers.length; i++ ) {
+
+    if (n = 0 <= 48.00 ) {
+        console.log("light fly");
+    }else if (n = 48.01 <= 51.00){
+        console.log("fly");
+    }else if (n = 51.01 <= 54.00){
+        console.log("bantam");
+    }else if (n = 54.01 <= 57.00){
+        console.log("feather");
+    }else if (n = 57.01 <= 60.00){
+        console.log("light");
+    }else if (n = 60.01 <= 64.00){
+        console.log("light welter");
+    }else if (n = 64.01 <= 69.00){
+        console.log("welter");
+    }else if (n = 69.01 <= 75.00){
+        console.log("light middle");
+    }else if (n = 75.01 <= 81.00){
+        console.log("middle");
+    }else if (n = 81.01 <= 91.00){
+        console.log("light heavy");
+    }else (n = 91.01 < Infinity){
+        console.log("heavy");
+    }
+}
