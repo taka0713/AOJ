80474e27fe9a1d1a9e93cd0f55b2b7a9f42687cb
taka0713 <tyunisu2018@gmail.com>
2022-05-23 01:35:10 +0900

    20220523

--- problem1.js
+++ problem1.js
@@ -1,9 +1,9 @@
 let n = require('fs').readFileSync('/dev/stdin', 'utf8');
 let numbers = (n.trim()).split("\n").map(Number);
-const f = function (a, b) {
-    return b - a
-}
-numbers.sort(f);
+
+numbers.sort(function(a,b){
+    return b - a;
+});
 console.log(numbers[0]);
 console.log(numbers[1]);
 console.log(numbers[2]);
--- problem48.js
+++ problem48.js
@@ -2,28 +2,27 @@ let n = require('fs').readFileSync('/dev/stdin', 'utf8');
 let numbers = (n.trim()).split("\n").map(Number);
 
 for (let i = 0; i < numbers.length; i++ ) {
-
-    if (0 <= n && n <=48.00 ) {
+    if (0 <= numbers[i] && numbers[i] <=48.00 ) {
         console.log("light fly");
-    }else if (48.01 <= n && n <= 51.00){
+    }else if (48.01 <= numbers[i] && numbers[i] <= 51.00){
         console.log("fly");
-    }else if (51.01 <= n && n <= 54.00){
+    }else if (51.01 <= numbers[i] && numbers[i] <= 54.00){
         console.log("bantam");
-    }else if (54.01 <= n && n <= 57.00){
+    }else if (54.01 <= numbers[i] && numbers[i] <= 57.00){
         console.log("feather");
-    }else if (57.01 <= n && n <= 60.00){
+    }else if (57.01 <= numbers[i] && numbers[i] <= 60.00){
         console.log("light");
-    }else if (60.01 <= n && n <= 64.00){
+    }else if (60.01 <= numbers[i] && numbers[i] <= 64.00){
         console.log("light welter");
-    }else if (64.01 <= n && n <= 69.00){
+    }else if (64.01 <= numbers[i] && numbers[i] <= 69.00){
         console.log("welter");
-    }else if (69.01 <= n && n <= 75.00){
+    }else if (69.01 <= numbers[i] && numbers[i] <= 75.00){
         console.log("light middle");
-    }else if (75.01 <= n && n <= 81.00){
+    }else if (75.01 <= numbers[i] && numbers[i] <= 81.00){
         console.log("middle");
-    }else if (81.01 <= n && n <= 91.00){
+    }else if (81.01 <= numbers[i] && numbers[i] <= 91.00){
         console.log("light heavy");
-    }else{(91.01 <= n)
+    }else{
         console.log("heavy");
     }
 }
